72cd2d1763739a48b6bcc46719683b3662afd226
jefzli21 <jefzli21@gmail.com>
2022-08-18 10:34:57 -0700

    done3

--- src/scripts/change.js
+++ src/scripts/change.js
@@ -1,15 +1,22 @@
 export async function change(){
     let fit = document.querySelector('#shape')
     let total_cal = parseInt(document.querySelector('#total-cal').innerText)
+    let total_fat = parseInt(document.getElementById('total-fat').innerText)
+
     if (total_cal < 900){
         const img = new Image();
         img.src = "./img/defaultHuman.png"
         img.id = "person"
         fit.appendChild(img)
-    }else if(total_cal >= 900 && total_cal < 10000){
+    }else if(total_cal >= 900 && total_cal < 10000 && total_fat < 50){
+        const img = new Image();
+        img.src = "./img/muscle.png"
+        img.id = "person"
+        fit.appendChild(img)
+    }else if(total_cal >= 900 && total_cal < 10000 && total_fat >= 50){
         const img = new Image();
         img.src = "./img/fatperson.png"
-        img.id = "person"
+        img.id = "person";
         fit.appendChild(img)
     }else{
         const img = new Image();
